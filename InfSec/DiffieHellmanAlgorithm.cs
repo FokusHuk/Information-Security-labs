32f089a8183cda93afbbe8b775b4d61b86249900
Ilya <ilyaaxenov1999@gmail.com>
2020-11-29 16:19:04 +0300

    DH algorithm

---
+++ InfSec/DiffieHellmanAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InfSec
+{
+    public class DiffieHellmanAlgorithm
+    {
+        public DiffieHellmanAlgorithm(double paramG, double paramP)
+        {
+            ParamG = paramG;
+            ParamP = paramP;
+        }
+
+        public void GenerateNumber()
+        {
+            var random = new Random();
+            LocalGeneratedParameter = random.Next(1, 10);
+        }
+        
+        public double CalculateParameterToSend()
+        {
+            return (Math.Pow(ParamG, LocalGeneratedParameter) % ParamP);
+        }
+
+        public void CalculateKey(double receivedParameter)
+        {
+            Key = (Math.Pow(receivedParameter, LocalGeneratedParameter) % ParamP);
+        }
+
+        public double ParamG { get; }
+        public double ParamP { get; }
+        public double LocalGeneratedParameter { get; private set; }
+        public double Key { get; private set; }
+    }
+}
--- InfSec/Program.cs
+++ InfSec/Program.cs
@@ -6,7 +6,43 @@ namespace InfSec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestDH();
+        }
+
+        static void TestDH()
+        {
+            Console.WriteLine("Diffie-Hellman Algorithm.");
+            
+            var paramG = 32.0;
+            var paramP = 40.0;
+
+            Console.WriteLine($"Parameter G: {paramG}\nParamter P: {paramP}\n");
+            
+            var Alice = new DiffieHellmanAlgorithm(paramG, paramP);
+            var Bob = new DiffieHellmanAlgorithm(paramG, paramP);
+            
+            Alice.GenerateNumber();
+            Bob.GenerateNumber();
+
+            Console.WriteLine($"Generated numbers:\n" +
+                              $"Alice: {Alice.LocalGeneratedParameter}\n" +
+                              $"Bob: {Bob.LocalGeneratedParameter}\n");
+
+            var AliceParameter = Alice.CalculateParameterToSend();
+            var BobParameter = Bob.CalculateParameterToSend();
+            
+            Console.WriteLine($"Calculated parameters:\n" +
+                              $"Alice: {AliceParameter}\n" +
+                              $"Bob: {BobParameter}\n");
+            
+            Alice.CalculateKey(BobParameter);
+            Bob.CalculateKey(AliceParameter);
+            
+            Console.WriteLine($"Calculated keys:\n" +
+                              $"Alice: {Alice.Key}\n" +
+                              $"Bob: {Bob.Key}\n");
+
+            Console.WriteLine($"Are keys equals: {Alice.Key == Bob.Key}");
         }
     }
 }
