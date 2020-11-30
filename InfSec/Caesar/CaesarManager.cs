f1f1badf318f6a2f24d1ef7e54a97dcbc7df12e8
Ilya <ilyaaxenov1999@gmail.com>
2020-11-30 11:15:10 +0300

    caesar manager added

---
+++ InfSec/Caesar/CaesarManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace InfSec.Caesar
+{
+    public class CaesarManager: IEncryptionAlgorithmManager
+    {
+        public CaesarManager()
+        {
+            Name = "Caesar";
+        }
+
+        public string Name { get; }
+        public void Execute()
+        {
+            var caesar = new CaesarAlgorithm();
+            var sourceText = "Sample text to encrypt";
+            var bias = 7;
+
+            Console.WriteLine($"Source text: {sourceText}\nBias: {bias}\n");
+            
+            var encryptedText = caesar.Encryption(sourceText, bias);
+            Console.WriteLine($"Encrypted text: {encryptedText}\n");
+            
+            var decryptedText = caesar.Decryption(encryptedText, bias);
+            Console.WriteLine($"Decrypted text: {decryptedText}\n");
+            
+            executeFrequncyDecryption();
+        }
+
+        private void executeFrequncyDecryption()
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Decryption by frequency (see the source file and base in the \"Test data\" folder).");
+            
+            var caesar = new CaesarAlgorithm();
+            var sourceText = FileManager.GetTextFromFile("./Test data/Caesar source text.txt");
+            var baseText = FileManager.GetTextFromFile("./Test data/Caesar base.txt");
+            var bias = 7;
+            
+            var encryptedText = caesar.Encryption(sourceText, bias);
+            Console.WriteLine($"Encrypted text:\n{encryptedText}\n");
+            
+            var decryptedText = caesar.DecryptionByFrequency(baseText, encryptedText);
+            Console.WriteLine($"Decrypted text:\n{decryptedText}\n");
+        }
+    }
+}
--- InfSec/Program.cs
+++ InfSec/Program.cs
@@ -10,26 +10,11 @@ namespace InfSec
         {
             var rsaManager = new RSAManager();
             var dhManager = new DHManager();
-            
-            var caesar = new CaesarAlgorithm();
-            
-            caesar.DecryptionByFrequency("aabcddd", "");
-            
-            var encr = caesar.Encryption("hello", 2);
-            var decr = caesar.Decryption(encr, 2);
-            Console.WriteLine($"{encr}\n{decr}");
-            
-            
-            var source = FileManager.GetTextFromFile("./Test data/Caesar source text.txt");
-            var baseText = FileManager.GetTextFromFile("./Test data/Caesar base.txt");
-
-            var enc = caesar.Encryption(source, 5);
-            var dec = caesar.DecryptionByFrequency(baseText, enc);
-
-            Console.WriteLine(dec);
+            var caesarManager = new CaesarManager();
 
             //rsaManager.Execute();
             //dhManager.Execute();
+            caesarManager.Execute();
             //ExecuteAll();
         }
 
@@ -38,7 +23,8 @@ namespace InfSec
             var algorithms = new IEncryptionAlgorithmManager[]
             {
                 new RSAManager(),
-                new DHManager()
+                new DHManager(),
+                new CaesarManager()
             };
 
             foreach (var algorithm in algorithms)
